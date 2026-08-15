910d0dae968f1f4b265024a210e4506680f8349d
Engr-Amir-khan <developerzone5000@gmail.com>
2026-08-15 15:33:33 +0500

    solve tower of hanoi question in gfg platform

---
+++ src/day31/TowerOfHanoi.java
@@ -0,0 +1,21 @@
+package day31;
+
+public class TowerOfHanoi {
+    static void main(String[] args) {
+        hanoi(3, 'A', 'B', 'C');
+    }
+
+    public static void hanoi(int n, char a, char b, char c) {
+
+        if(n==0) return;
+
+        hanoi(n-1, a, c, b);
+        System.out.println(a + " => " + c);
+        hanoi(n-1, b,a,c);
+
+    }
+
+
+
+
+}
